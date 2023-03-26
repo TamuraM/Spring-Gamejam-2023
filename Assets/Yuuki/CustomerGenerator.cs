83600aa5ffcc2f02d21b6a9223efbf29f7ad97ee
Ray <rei.piyo.55687@gmail.com>
2023-03-26 17:33:30 +0900

    [Update]男女比を調整しやすいように変更,オーダーの種類を追加

--- Assets/Yuuki/CustomerGenerator.cs
+++ Assets/Yuuki/CustomerGenerator.cs
@@ -7,21 +7,19 @@ using UnityEngine.UI;
 /// <summary> 次に来店する客を生成するクラス </summary>
 public class CustomerGenerator : MonoBehaviour
 {
-    [SerializeField, Tooltip("客のプレハブ")] GameObject[] customers = new GameObject[6];
+    [SerializeField, Tooltip("客のプレハブ(前半が女子・後半が男子)")] GameObject[] customers = new GameObject[6];
     [SerializeField, Tooltip("オーダーのボーダー")] int _orderBorder = default;
     [SerializeField, Tooltip("オーダーのセリフテキスト")] Text _orderText = default;
     [Header("オーダーのセリフ内容")]
-    [SerializeField, Tooltip("可愛い")] string _cuteLogue = default;
-    [SerializeField, Tooltip("かっこいい")] string _coolLogue = default;
-    [SerializeField, Tooltip("面白い")] string _amusingLogue = default;
-    [SerializeField, Tooltip("セクシー")] string _sexyLogue = default;
-    [SerializeField, Tooltip("怖い")] string _ghostlyLogue = default;
+    [SerializeField] String[] diaLogues = new string[Enum.GetNames(typeof(NextOrder)).Length];
+    [SerializeField, Tooltip("女性の割合(整数％)")] int _womenRatio = default;
 
     /// <summary> 次の客を生成する。Decisionから呼ばれる </summary>
     public void Generate()
     {
         _order = DecideOrder(); //オーダーを決定する
         _orderText.text = Dialogue(_order); //セリフを表示する
+        SetParameter(_order);
         DecideCusttomer().SetActive(true);  //客を表示する
     }
 
@@ -34,6 +32,21 @@ public class CustomerGenerator : MonoBehaviour
         Amusing = 3,
         Sexy = 4,
         Ghostly = 5,
+
+        CuteAndCool = 6,
+        CuteAndAmusing = 7,
+        CuteAndSexy = 8,
+        CuteAndGhostly = 9,
+
+        CoolAndAmusing = 10,
+        CoolAndSexy = 11,
+        CoolAndGhostly = 12,
+
+        AmusingAndSexy = 13,
+        AmusingAndGhostly = 14,
+
+        SexyAndGhostly = 15,
+
     }
     NextOrder _order = NextOrder.Uncertainty;
     
@@ -41,8 +54,9 @@ public class CustomerGenerator : MonoBehaviour
     /// <returns></returns>
     private NextOrder DecideOrder()
     {
-        int orderIndex = UnityEngine.Random.Range(0, 4);
-        //ランダムにオーダーを生成するために、Enumをintに変換
+        //ランダムにオーダーを生成する
+        int orderIndex = UnityEngine.Random.Range(0, Enum.GetNames(typeof(NextOrder)).Length);
+        //Enumをintに変換
         NextOrder next = (NextOrder)Enum.ToObject(typeof(NextOrder), orderIndex);
         return next;
     }
@@ -58,15 +72,39 @@ public class CustomerGenerator : MonoBehaviour
             case NextOrder.Amusing: Decision.Instance.Customer._amuseing = _orderBorder; break;
             case NextOrder.Sexy: Decision.Instance.Customer._sexy = _orderBorder; break;
             case NextOrder.Ghostly: Decision.Instance.Customer._ghostly = _orderBorder; break;
+
+            case NextOrder.CuteAndCool: Decision.Instance.Customer._cute = _orderBorder; Decision.Instance.Customer._cool = _orderBorder; break;
+            case NextOrder.CuteAndAmusing:Decision.Instance.Customer._cute = _orderBorder; Decision.Instance.Customer._amuseing = _orderBorder; break;
+            case NextOrder.CuteAndSexy: Decision.Instance.Customer._cute = _orderBorder; Decision.Instance.Customer._sexy = _orderBorder; break;
+            case NextOrder.CuteAndGhostly: Decision.Instance.Customer._cute = _orderBorder; Decision.Instance.Customer._ghostly = _orderBorder; break;
+
+            case NextOrder.CoolAndAmusing: Decision.Instance.Customer._cool = _orderBorder; Decision.Instance.Customer._amuseing = _orderBorder; break;
+            case NextOrder.CoolAndSexy: Decision.Instance.Customer._cool = _orderBorder; Decision.Instance.Customer._sexy = _orderBorder; break;
+            case NextOrder.CoolAndGhostly: Decision.Instance.Customer._cool = _orderBorder; Decision.Instance.Customer._ghostly = _orderBorder; break;
+
+            case NextOrder.AmusingAndSexy: Decision.Instance.Customer._amuseing = _orderBorder; Decision.Instance.Customer._sexy= _orderBorder; break;
+            case NextOrder.AmusingAndGhostly: Decision.Instance.Customer._amuseing = _orderBorder; Decision.Instance.Customer._ghostly = _orderBorder; break;
+
+            case NextOrder.SexyAndGhostly: Decision.Instance.Customer._sexy = _orderBorder; Decision.Instance.Customer._ghostly = _orderBorder; break;
+
+
             default: break;
         }
-
     }
 
     /// <summary> 客のプレハブを決定する </summary>
     private GameObject DecideCusttomer()
     {
-        int customerIndex = UnityEngine.Random.Range(0, 5);
+        int lot = UnityEngine.Random.Range(0, 100);
+        int customerIndex = default;
+        if (lot < _womenRatio)
+        {
+            customerIndex = UnityEngine.Random.Range(0, 3);
+        }
+        else
+        {
+            customerIndex = UnityEngine.Random.Range(4, 6);
+        }
         return customers[customerIndex];
     }
 
@@ -77,11 +115,26 @@ public class CustomerGenerator : MonoBehaviour
     {
         switch(nextOrder)
         {
-            case NextOrder.Cute: return _cuteLogue;
-            case NextOrder.Cool: return _coolLogue;
-            case NextOrder.Amusing: return _amusingLogue;
-            case NextOrder.Sexy: return _sexyLogue;
-            case NextOrder.Ghostly: return _ghostlyLogue;
+            case NextOrder.Cute: return diaLogues[0];
+            case NextOrder.Cool: return diaLogues[1];
+            case NextOrder.Amusing: return diaLogues[2];
+            case NextOrder.Sexy: return diaLogues[3];
+            case NextOrder.Ghostly: return diaLogues[4];
+
+            case NextOrder.CuteAndCool:return diaLogues[5];
+            case NextOrder.CuteAndAmusing: return diaLogues[6];
+            case NextOrder.CuteAndSexy: return diaLogues[7];
+            case NextOrder.CuteAndGhostly: return diaLogues[8];
+
+            case NextOrder.CoolAndAmusing: return diaLogues[9];
+            case NextOrder.CoolAndSexy: return diaLogues[10];
+            case NextOrder.CoolAndGhostly: return diaLogues[11];
+
+            case NextOrder.AmusingAndSexy: return diaLogues[12];
+            case NextOrder.AmusingAndGhostly: return diaLogues[13];
+
+            case NextOrder.SexyAndGhostly: return diaLogues[14];
+
             default: return "";
         }
     }
